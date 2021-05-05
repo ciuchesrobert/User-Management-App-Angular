fdfa01243f362ae3da5c40bfb728acfaa211e215
ciuches.robert <ciuches.robert@yahoo.com>
2021-05-05 14:51:46 +0300

    login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,10 @@ import {AuthInterceptor} from './interceptor/auth.interceptor';
 import {AuthenticationGuard} from './guard/authentication.guard';
 import {NotificationModule} from './notification.module';
 import {NotificationService} from './service/notification.service';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { UserComponent } from './user/user.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {UserComponent} from './user/user.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -24,8 +25,9 @@ import { UserComponent } from './user/user.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     HttpClientModule,
-    NotificationModule
+    NotificationModule,
   ],
   providers: [
     AuthenticationGuard,
